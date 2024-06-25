60558641eeb51ca0f7a7422fb583c18c65dd4e85
niño tesoro <na.tesoro@gmail.com>
2024-06-25 04:10:41 +0000

    initial commits for this activity

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import time_series_visualizer
+import sys
 from unittest import main
+
+print (sys.argv[1:])
 
 # Test your function by calling it here
 time_series_visualizer.draw_line_plot()
-time_series_visualizer.draw_bar_plot()
-time_series_visualizer.draw_box_plot()
+#time_series_visualizer.draw_bar_plot()
+#time_series_visualizer.draw_box_plot()
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
